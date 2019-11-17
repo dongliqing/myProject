0449c34e6ea274f43cb87ac868b9ca87ffe52963
dongliqing <18758053497@163.com>
2019-11-17 13:25:45 +0800

    vue project

---
+++ src/views/home.vue
@@ -0,0 +1,29 @@
+<style lang="less" scoped>
+  .box{
+    width:150px;
+    height:150px;
+    position: relative;
+    
+  }
+</style>
+
+<template>
+  <div>
+      <div class="box">
+        <div></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+
